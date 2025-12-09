5a55e7ae0247920400e074ad392da3b258577013
gilbertsize112 <jamesize81@gmail.com>
2025-12-09 10:17:01 +0100

    Updated files

--- models/HateAlert.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const HateAlertSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  url: { type: String, default: "" },
-  timestamp: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("HateAlert", HateAlertSchema);
