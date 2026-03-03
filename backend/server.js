1eb58cb5abcb1eac0dfc40fb4aee7cf5c114c6ff
gilbertsize112 <jamesize81@gmail.com>
2026-03-03 21:29:21 +0100

    Updated files

--- backend/server.js
+++ backend/server.js
@@ -135,8 +135,7 @@ app.use(
         allowedHeaders: ["Content-Type", "Authorization"]
     })
 );
-app.options("*", cors());
-
+app.options("/(.*)", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
