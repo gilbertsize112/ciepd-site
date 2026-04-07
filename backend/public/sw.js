ddaaa110f737df5e0c56cf0397942d917de7dca4
gilbertsize112 <jamesize81@gmail.com>
2026-04-07 09:38:48 +0100

    Updated files

---
+++ backend/public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'ciepd-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/login.html', '/logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
